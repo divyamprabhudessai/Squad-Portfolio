bd08a85770db635de34c61b45db9df02af59823d
divyamprabhudessai <divyamprabhudessai26@gmail.com>
2024-08-19 16:43:21 +0530

    homepage frontend

--- Frontend/squad-portfolio/src/components/Home/Home.jsx
+++ Frontend/squad-portfolio/src/components/Home/Home.jsx
@@ -45,14 +45,185 @@ export default function Home() {
                         <div className="strip"></div>
                         <div className="strip"></div>
                     </div>
-                  
+
                 </div>
 
 
+                <div className="content-section">
+                    <div>
+                        <h2 id="statistics-title">Squad-statistics</h2>
+                    </div>
+                    <div class="grid-container">
+                        <div class="grid-item item1">
+                            <div className="item1-content"> <span className="span"><h2>16 </h2> Members</span></div>
+                            <div >
+                                <h2>30k +   </h2>
+                                Hours worked
+                            </div>
+                        </div>
+                        <div class="grid-item item2">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div class="grid-item item3">hello</div>
+                        {/* <div class="grid-item item4">hello</div> */}
+                    </div>
+                </div>
 
+                <h1 id="squad-members-title">Squad-Members</h1>
+                <div className="squad-members-section">
+                    
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
 
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
 
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
+
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
+
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
+
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
+
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
+                    <div class="card">
+                        <div class="image-container">
+                            <img src="https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg" alt="" />
+                        </div>
+                        <div>
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Member-1</h3>
+                            <p>UI design - User research - <br /> Webflow develop</p>
+                        </div>
+                        <div class="arrow">
+                            <button id="arrow-btn">
+                            <span id="act-arrow"> &#8599;</span>
+
+                            </button>
+                            
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+                <div className="connect">
+                    <h2>Let's connect</h2>
+                    <p>Want to connect and know more about us?</p>
+                    <button>Send us a message</button>
+                </div>
+
+                
+                    <footer>
+                        <div className="foot-cont">
+                            <div className="bot-logo">
+                            <img src="https://img.hotimg.com/logo7506d83569140886.png" alt="" id="logo" />
+                            <p>55</p>
+                            </div>
+                             <p> Portfolio 2024</p>
+                             <div className="social">
+                                <img src="https://img.hotimg.com/icon-park-outline_dribble.png" alt="" />
+                                <img src="https://img.hotimg.com/ri_instagram-line.png" alt="" />
+                                <img src="https://img.hotimg.com/akar-icons_x-fill.png" alt="" />
+                                    
+                             </div>
+                        </div>
+                    </footer>
+            
             </div>
+
         </>
     )
 };
