b5e7d2f78776ee313c524812b76d423eae3f7120
divyamprabhudessai <divyamprabhudessai26@gmail.com>
2024-09-01 21:39:51 +0530

    changes

--- Frontend/squad-portfolio/src/App.jsx
+++ Frontend/squad-portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './components/Home/Home'
+import LoadingPage from './components/Home/LoadingPage'
 import './App.css'
 
 function App() {
   return (
     <>
       < Home/>
+      {/* <LoadingPage/> */}
     </>
   )
 }
